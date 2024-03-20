3ea824bf05fb8a7d9c915fd02f5368459fbeed9e
Jacob Felding <jfelding@gmail.com>
2024-03-20 13:25:51 +0100

    cleanup for POC

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,7 +2,7 @@ export function dbLogin(email: string, password: string) {
   let username: string | null = null;
   let success: boolean = true;
   return new Promise((resolve, reject) => {
-    if (email === "arne@repaircafedanmark.dk" && password === "1234") {
+    if (email === "test@repaircafedanmark.dk" && password === "test") {
       username = "Repair Cafe Nørrebrohallen";
     } else {
       success = false;
